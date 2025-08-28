5a71671b8ac7073458d8ffc8de0ae52a0787b11d
Adrián Vančo <55744976+vancoadrian@users.noreply.github.com>
2025-08-28 13:52:29 +0200

    baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      baseURL: process.env.NUXT_APP_BASE_URL || '/',
+      baseURL: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
     }
   },
 })
