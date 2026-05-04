5ec95dbc334adb31f20be3a87bcf97be963e0667
Adrián Vančo <55744976+vancoadrian@users.noreply.github.com>
2026-05-04 15:56:07 +0200

    change featured

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -21,6 +21,7 @@ export const projectCategoriesData: ProjectCategoryData[] = [
         nameKey: 'projectsData.items.carServiceWebsite.name',
         descriptionKey: 'projectsData.items.carServiceWebsite.description',
         link: '#',
+        featured: true,
         images: [
           '/webs/car-service/desktop/homecookies.png',
           '/webs/car-service/desktop/contact.png',
@@ -93,6 +94,7 @@ export const projectCategoriesData: ProjectCategoryData[] = [
         nameKey: 'projectsData.items.chatRooms.name',
         descriptionKey: 'projectsData.items.chatRooms.description',
         link: '#',
+        featured: true,
         images: [
           '/chatapp/rooms.png',
           '/chatapp/login.png',
@@ -135,6 +137,7 @@ export const projectCategoriesData: ProjectCategoryData[] = [
         nameKey: 'projectsData.items.xenergie.name',
         descriptionKey: 'projectsData.items.xenergie.description',
         link: '#',
+        featured: true,
         images: ['/microstep/variostepone/treeview.png', '/microstep/variostepone/anomalydef.png'],
       },
       {
@@ -173,7 +176,6 @@ export const projectCategoriesData: ProjectCategoryData[] = [
         nameKey: 'projectsData.items.serverAdministration.name',
         descriptionKey: 'projectsData.items.serverAdministration.description',
         link: '#',
-        featured: true,
         images: ['/proxenta/vmware.png'],
       },
       { nameKey: 'projectsData.items.ms365Administration.name', descriptionKey: 'projectsData.items.ms365Administration.description', link: '#' },
@@ -182,7 +184,6 @@ export const projectCategoriesData: ProjectCategoryData[] = [
         nameKey: 'projectsData.items.activeDirectoryMigration.name',
         descriptionKey: 'projectsData.items.activeDirectoryMigration.description',
         link: '#',
-        featured: true,
         images: ['/proxenta/ad/info.png'],
       },
       {
@@ -190,7 +191,6 @@ export const projectCategoriesData: ProjectCategoryData[] = [
         descriptionKey: 'projectsData.items.fileserverPermissionsMigration.description',
         link: '#',
         images: ['/proxenta/ad/groups.png'],
-        featured: true,
       },
       { nameKey: 'projectsData.items.nasBackups.name', descriptionKey: 'projectsData.items.nasBackups.description', link: '#' },
       { nameKey: 'projectsData.items.domainManagement.name', descriptionKey: 'projectsData.items.domainManagement.description', link: '#' },
